cdfb1c073f1c496d9b9435f5a605b477657820d4
Momen959 <momen.saif22@gmail.com>
2026-01-05 00:48:25 +0200

    revert

--- Server/src/models/Group.js
+++ Server/src/models/Group.js
@@ -15,17 +15,6 @@ const groupSchema = new mongoose.Schema({
     capacity: { 
         type: Number, 
         required: true 
-    },
-    grade: {
-        type: String,
-        enum: ['KG1', 'KG2', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'G10', 'G11', 'G12'],
-        required: true
-    },
-    educationType: {
-        type: String,
-        enum: ['local', 'azhar', 'national', 'international'],
-        required: true,
-        lowercase: true
     }
 }, { timestamps: true });
 
